068676a1ed7eec2e26f36c6084eb1ac64c11dcb4
Maksim Kostromin <daggerok@gmail.com>
2017-11-04 02:19:50 +0200

    fix data

--- elastic/data/src/main/java/daggerok/elastic/User.java
+++ elastic/data/src/main/java/daggerok/elastic/User.java
@@ -7,19 +7,16 @@ import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
 @Accessors(chain = true)
-@Document(indexName = "user", type = "customer")
-//@Document(indexName = "daggerok", type = "user")
-//@Document(indexName = "user", type = "user", shards = 1, replicas = 0, refreshInterval = "-1")
-//@Document(indexName = "user")
-//@Document(
-//    indexName = "user-index",
-//    indexStoreType = "mmapfs",
-//    type = "user",
-//    replicas = 0,
-//    shards = 1,
-//    createIndex = true,
-//    refreshInterval = "-1"
-//)
+//@Document(indexName = "user", type = "customer")
+@Document(
+    indexName = "user",
+    type = "customer",
+    indexStoreType = "mmapfs",
+    replicas = 1,
+    shards = 3,
+    createIndex = true,
+    refreshInterval = "-1"
+)
 public class User {
 
   @Id
