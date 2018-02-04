d2bd9b9076ebf856a859dd080eddd4326d042b3b
Maksim Kostromin <daggerok@gmail.com>
2018-02-05 01:05:33 +0200

    init infinispan-example

---
+++ infinispan-example/infinispan-service/src/main/java/daggerok/InfinispanServiceApplication.java
@@ -0,0 +1,12 @@
+package daggerok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinispanServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InfinispanServiceApplication.class, args);
+  }
+}
